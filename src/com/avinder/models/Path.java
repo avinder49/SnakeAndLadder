3d5578972b39ac2cd881cef0a15f6c3c536bde95
Avinder Singh <avinder49@gmail.com>
2022-05-16 21:07:02 +0530

    Added Ladder, Triple 6 logic

--- src/com/avinder/models/Path.java
+++ src/com/avinder/models/Path.java
@@ -1,14 +1,19 @@
 package com.avinder.models;
 
 public class Path {
+    private int from;
+    private int to;
+    public int getFrom() {return from;}
+    public void setFrom(int from) {this.from = from;}
+    public int getTo() {return to;}
+    public void setTo(int to) {this.to = to;}
+
     @Override
     public String toString() {
         return "Path{" + from +
                 "->" + to + '}';
     }
 
-    int from;
-    int to;
     Path(int from , int to){
         this.from = from;
         this.to = to;
